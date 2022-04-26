8ef390f97e6d5f1ef6de4169d1d0ebccdec6db41
Mesi <mse212002@gmail.com>
2022-04-26 11:49:27 +0300

    :sparkles: rendered todos

--- src/Components/TodoContainer.js
+++ src/Components/TodoContainer.js
@@ -3,14 +3,40 @@ import React, { Component } from 'react';
 class TodoContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'First item',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Second item',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Third item',
+          completed: false,
+        },
+      ],
+    };
   }
 
   render() {
+    const { todos } = this.state;
+
     return (
       <div>
         <h1>Hello Todos</h1>
         <p>I am in the Container Component</p>
+        <ul>
+          {todos.map((todo) => {
+            const { title, id } = todo;
+            return <li key={id}>{title}</li>;
+          })}
+        </ul>
       </div>
     );
   }
